97fe71d4692ad00fbe7be48c6d37941bb08cb55d
Edwin Encomienda <encomienda000@gmail.com>
2017-11-20 16:58:35 +0800

    gitignore

---
+++ immutable.js
@@ -0,0 +1,11 @@
+const immutable = {
+  push (state, data) {
+    return typeof(data) === 'string' ? [...state, data] : [...state].concat(data)
+  },
+  delete (state, id) {
+    return state.filter(item => item.id !== id)
+  },
+  update (state, data) {
+    return state.map(item => item.id !== data.id ? item : data )
+  }
+}
